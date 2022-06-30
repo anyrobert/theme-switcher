91e76c8baae6f6a1c1028a172f60893fcbc97f8b
Robert Gabriel <anyrobert@outlook.com.br>
2022-06-30 12:16:16 -0300

    move usePersistedState to hooks folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, {useCallback}  from 'react';
 import {ThemeProvider, DefaultTheme} from 'styled-components';
-import usePersistedState from './utils/usePersistedState';
+import usePersistedState from './hooks/usePersistedState';
 
 import { HeaderProvider } from './HeaderContext';
 
